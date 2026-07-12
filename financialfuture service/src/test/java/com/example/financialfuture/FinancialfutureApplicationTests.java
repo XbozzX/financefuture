76c103f68b54eea5a458a6c9328b7ffc8fac4378
XbozzX <afham821@gmail.com>
2026-07-12 23:03:53 +0800

    initial project

---
+++ financialfuture service/src/test/java/com/example/financialfuture/FinancialfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
